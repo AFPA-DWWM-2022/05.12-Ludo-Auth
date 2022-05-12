b6d9e5bc5de58685d216e4dcd399bf1cc63db96c
Renard <ludofernandez@msn.com>
2022-05-12 22:55:58 +0200

    feat(MVC): Implement user registration (full stack)

---
+++ controller/auth.js
@@ -0,0 +1,50 @@
+/*
+ * auth.js
+ * Copyright (C) 2022 Ludovic Fernandez <http://github.com/SirWrexes>
+ *
+ * Distributed under terms of the MIT license.
+ */
+'use strict';
+
+const { ifError } = require('assert');
+const bcrypt = require('bcrypt');
+
+const { createUsers } = require('../db/index');
+
+const _RE = {
+  /** At least 8 characters, with at least one occurence of {alpha, num, special} */
+  pass: /^(?=.*\w)(?=.*\d)(?=.*\W).{8,}$/,
+
+  /** Only one @, at least one character on each side, and a . at the end*/
+  mail: /^[^@]+@[^@]+\./
+};
+
+module.exports = {
+  signUp: async (req, res) => {
+    var e = undefined;
+    const { name, mail, pass, chck } = req.body;
+
+    console.log(_RE.mail.test('jobijoba@job.com'));
+    console.log(mail, name, pass, chck);
+
+    if (!_RE.mail.test(mail)) {
+      e = 'Invalid email !';
+    } else if (!_RE.pass.test(pass)) {
+      e = 'Invalid password !';
+    } else if (pass !== chck) {
+      e = 'Password/Confirmation mismatch !';
+    }
+
+    ifError(e);
+
+    createUsers({
+      name,
+      mail,
+      pass: await bcrypt.hash(pass, process.env.HASH_SALT),
+    });
+
+    res.status(200).send('GG gros gaillard aguerri !');
+  },
+
+  singIn: undefined,
+};
--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,4 @@
 module.exports = require('express')
   .Router()
   .get('/register', (_, res) => { res.render('register'); })
-  .post('/register', (req, res) => {
-    console.log(req.url);
-    console.log(req.body);
-    res.send('router/auth: Registration ok!');
-  });
+  .post('/register', require('../controller/auth').signUp);
