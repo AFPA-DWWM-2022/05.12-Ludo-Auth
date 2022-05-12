4dce7b1a8cb25b17e0f98a05466b05778e614c00
Renard <ludofernandez@msn.com>
2022-05-12 22:55:05 +0200

    refactor: Slightly improve snippet for salt generation

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ require('dotenv').config({
 
 const { appendFile } = require('fs');
 const { join } = require('path');
-const bcrypt = require('bcrypt');
+const { genSalt } = require('bcrypt');
 
 const server = require('./server');
 const db = require('./db/index');
+const { ifError } = require('assert');
 
 async function main() {
   if (!process.env.HASH_SALT) {
     const rounds = parseInt(process.env.HASH_ROUNDS || '12');
-    const salt = bcrypt.genSalt(rounds);
+    const salt = genSalt(rounds);
 
-    appendFile(join(__dirname, '.env'), `HASH_SALT=${JSON.stringify(await salt)}`, e => e && console.log(e));
+    appendFile(
+      join(__dirname, '.env'),
+      `HASH_SALT=${JSON.stringify(await salt)}`,
+      ifError
+    );
     process.env.HASH_SALT = salt;
   }
   db.connect();
